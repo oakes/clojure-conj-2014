7f96ce9a1be2586a9c9191d7a18c0ff411086e50
oakes <zsoakes@gmail.com>
2014-09-23 16:10:08 -0400

    Add title slide

--- core.clj
+++ core.clj
@@ -2,7 +2,14 @@
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage) :camera (orthographic))
-    (label "Hello world!" (color :white)))
+    (let [ui-skin (skin "skin/uiskin.json")]
+      (table [(label "Making Games\nat Runtime\nwith Clojure" ui-skin
+                     :set-alignment (align :center))
+              :row
+              [(label "by Zach Oakes" (color :white))
+               :pad-top 10]]
+             :align (align :center)
+             :set-fill-parent true)))
   
   :on-render
   (fn [screen entities]
@@ -11,6 +18,6 @@
   
   :on-resize
   (fn [screen entities]
-    (height! screen (:height screen))))
+    (height! screen 400)))
 
 (set-game-screen! main-screen)
