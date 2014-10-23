26d13a6d01057b9a2dbb6d84b0e18ce4e9e1cfb8
oakes <zsoakes@gmail.com>
2014-10-23 15:15:41 -0400

    Increase font sizes

--- 01-title.clj
+++ 01-title.clj
@@ -2,14 +2,16 @@
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage) :camera (orthographic))
-    (let [ui-skin (skin "skin/uiskin.json")]
+    (let [ui-skin (skin "skin/uiskin.json")
+          small-font (skin! ui-skin :get-font "small-font")
+          small-style (style :label small-font (color :white))]
       (table [(label (str "Making Games" \newline
                           "at Runtime" \newline
                           "with Clojure")
                      ui-skin
                      :set-alignment (align :center))
               :row
-              [(label "by Zach Oakes" (color :white))
+              [(label "by Zach Oakes" small-style)
                :pad-top 10]]
              :align (align :center)
              :set-fill-parent true)))
--- 02-quote.clj
+++ 02-quote.clj
@@ -2,11 +2,13 @@
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage) :camera (orthographic))
-    (let [ui-skin (skin "skin/uiskin.json")]
+    (let [ui-skin (skin "skin/uiskin.json")
+          small-font (skin! ui-skin :get-font "small-font")
+          small-style (style :label small-font (color :white))]
       (table [(label "It's all in the game." ui-skin
                      :set-alignment (align :center))
               :row
-              [(label "Omar Little" (color :white))
+              [(label "Omar Little" small-style)
                :pad-top 10]]
              :align (align :center)
              :set-fill-parent true)))
