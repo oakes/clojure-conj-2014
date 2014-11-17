bf2abc81f6ba4f8d5dd3377c134863fca9b4b83f
oakes <zsoakes@gmail.com>
2014-11-17 17:23:54 -0500

    Remove unused lines

--- 35-quest-quest.clj
+++ 35-quest-quest.clj
@@ -3,47 +3,7 @@
 (def quests
   [{:title "Welcome to Quest Quest!"
     :body ""
-    :reward "Falling Unlocked!"}
-
-   {:title "Safety First"
-    :body "The ground is fast approaching, you must land safely!"
-    :reward "Move Right Unlocked!"}
-
-   {:title "Right of Way"
-    :body "Get a feel for your surroundings, press d or right arrow to move right as far as your legs will carry you!"
-    :reward "Move Left Unlocked!"}
-
-   {:title "Left Alone"
-    :body "Continue to explore your new surroundings, press press a or left arrow to move left!"
-    :reward "Attack Unlocked!"}
-
-   {:title "A Trial By Combat"
-    :body "Your first enemy blocks the path! You must defeat it to continue. Apply everything you've learned so far to vanquish this beast!"
-    :reward "Leveling Up Has Made You Stronger!"}
-
-   {:title "Another Trial By Combat"
-    :body "A scarier enemiy blocks your path! Vanquish it to proceed!"
-    :reward "Leveling Up Has Made You Stronger!"}
-
-   {:title "An Elite Leader"
-    :body "An extremely cunning and ferocious stands between you and the nearby village! Fight with all your power to overcome it!"
-    :reward "Pick-up Item Unlocked!"}
-
-   {:title "Johnney Applegatherer"
-    :body "Those villagers look very hungry. Gather all of their apples to keep them from starving to death!"
-    :reward "Jump Unlocked!"}
-
-   {:title "Launch Over It!"
-    :body "Tighten the muscles in your legs to form a spring and launch yourself over the rock."
-    :reward "Jump-Attack Unlocked!"}
-
-   {:title "The Epic Raid Boss"
-    :body "Destroy the final boss to see what lies inside the treasure room!"
-    :reward "Open Treasure Unlocked!"}
-
-   {:title "End Game"
-    :body "Congratulations, you've reached the peak of your power! Surely untold adventures lie before you."
-    :reward "End-Game Content Unlocked!"}])
+    :reward "Falling Unlocked!"}])
 
 ; ui
 
